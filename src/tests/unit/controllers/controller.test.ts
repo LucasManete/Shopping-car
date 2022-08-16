ada731f469ab70169dc43c3f41da196c54bab8c9
LucasManete <lucas.manete2403@gmail.com>
2022-08-16 19:16:24 -0300

    feat: 15% cobertura na controller

---
+++ src/tests/unit/controllers/controller.test.ts
@@ -0,0 +1,41 @@
+// template para criação dos testes de cobertura da camada de controller
+
+
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { NextFunction, Request, Response } from 'express';
+import CarModel from '../../../models/carsModel';
+import CarService from '../../../services/carService';
+import CarController from '../../../controllers/CarController';
+import { carMock } from '../mocks/carMock';
+
+
+describe('Camanda Controller', () => {
+    const carModel = new CarModel();
+    const carService = new CarService(carModel);
+    const carController = new CarController(carService);
+    const req = {} as Request;
+    const res = {} as Response;
+  before(async () => {
+    sinon
+      .stub(carService, 'create')
+      .resolves(carMock);
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns(res)
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('Create Car', () => {
+    it("Sucesso", async () => {
+        req.body = carMock
+        await carController.create(req, res);
+        expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+        expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    })
+  });
+
+});
