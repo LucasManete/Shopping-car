7de5c17a913dec232b986c61ef8995448e62341a
LucasManete <lucas.manete2403@gmail.com>
2022-08-16 15:51:11 -0300

    feat: Crie a interface ICar a partir da interface IVehicle

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { vehicleSchema } from './IVehicle';
+
+const carSchema = vehicleSchema.extend({
+  doorsQty: z.number({}).min(2, { message: 'Deve ser maior ou igual a 2' })
+    .max(4, { message: 'Deve ser menor ou igual a 4' }),
+  seatsQty: z.number({}).min(2, { message: 'Deve ser maior ou igual a 2' })
+    .max(7, { message: 'Deve ser menor ou igual a 7' }),
+});
+
+type ICar = z.infer<typeof carSchema>;
+
+export { ICar, vehicleSchema };
